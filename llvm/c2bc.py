03c35a3d3b47611f8fb59c46c0f5fa8782cf4564
LCID Fire <lcid-fire@gmx.net>
2011-09-26 15:26:05 +0200

    Switching from llvm-gXX to dragonegg. It seems like dragonegg cannot generate llvm bitcode anymore so we use a python script to convert llvm ir to bitcode (<- slow)

---
+++ llvm/c2bc.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+import sys, subprocess
+
+#def which(cmd):
+#	return subprocess.check_output('which ' + cmd, executable='/bin/bash',shell=True).rstrip('\n\r')
+
+args = list(sys.argv)
+del args[0] # Remove our exec name
+compiler = args[0] 
+del args[0] # Remove the compile name
+
+#compiler = which(compiler)
+#compiler = which.which(compiler)
+
+compileArguments = [compiler]
+outputFile = ''
+
+foundFile = False
+
+for arg in args:
+	if arg.startswith('-o'):
+		foundFile = True
+	elif foundFile:
+		outputFile = arg
+		foundFile = False
+		arg = "/dev/stdout"
+
+	compileArguments.append(arg)
+
+#compileArguments.append('-S')
+
+#print compileArguments
+
+compileProcess = subprocess.Popen(compileArguments, stdout=subprocess.PIPE)
+#print outputFile
+asbin = '/home/andreas/bin/llvm-as'
+
+#asbin = which(asbin)
+#print asbin
+#asbin = which.which(asbin)
+
+ir2bcProcess = subprocess.Popen([asbin, '-f', '-o=' + outputFile], stdin=compileProcess.stdout)
+
+#stdout, stderr = compileProcess.communicate()
+stdout, stderr = ir2bcProcess.communicate()
+
+#if stdout != None:
+#	print "OUT START"
+#	print stdout
+#	print "OUT END"
+
+#if stderr != None:
+#	print "ERR START"
+#	print stderr
+#	print "ERR END"
+
+compileProcess.wait()
+ir2bcProcess.wait()
+
+
+#print data
+#(stdoutdata, stderrdata)
+
