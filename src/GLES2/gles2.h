825c88af6af275df089ac5be6b8c2960838d6f4d
LCID Fire <lcid-fire@gmx.net>
2011-09-26 15:28:46 +0200

    Move template stuff to gles2 header so templates are instantiable

--- src/GLES2/gles2.h
+++ src/GLES2/gles2.h
@@ -3,7 +3,6 @@
 
 #include "emul/include.h"
 
-#ifdef __cplusplus
 #include "emul/alpha.h"
 #include "emul/clip.h"
 #include "emul/color.h"
@@ -18,6 +17,73 @@
 #include "emul/texture.h"
 #include "emul/vertex.h"
 
-#endif //__cplusplus
+template<typename... TYPES>
+class Setup
+{
+private:
+	struct FalseType { enum { value = false }; };
+	struct TrueType  { enum { value =  true }; };
+
+	template<typename T1, typename T2>
+	struct IsSame
+	{
+		typedef FalseType Result;
+	};
+
+	template <typename T>
+	struct IsSame<T,T>
+	{
+		typedef TrueType Result;
+	};
+
+	template<GLubyte INDEX, typename TEST, typename T1>
+	GLubyte GetIndex()
+	{
+		if( IsSame<TEST, T1>::Result::value )
+			return INDEX;
+		
+		throw "Type not registered";
+	}
+
+	template<GLubyte INDEX, typename TEST, typename T1, typename T2, typename... TOTHER>
+	GLubyte GetIndex()
+	{
+		if( IsSame<TEST, T1>::Result::value )
+			return INDEX;
+		
+		return GetIndex<INDEX + 1, TEST, T2, TOTHER...>();
+	};
+public:
+	template<typename T>
+	T& Get()
+	{
+		GLubyte byte = GetIndex<0, T, TYPES...>();
+		return *(new T());
+	};
+};
+
+typedef Setup<
+	AlphaSetup,
+	ClipSetup,
+	ColorSetup,
+	FogSetup,
+	LightSetup,
+	LineSetup,
+	MaterialSetup,
+	NormalSetup,
+	PointSetup,
+	StateSetup,
+	TextureSetup,
+	VertexSetup
+> GLES2Setup ;
+
+extern GLES2Setup setup; 
+
+template<typename T>
+T& Get()
+{
+	return setup.Get<T>();
+}
+
 #endif //_HW_GLES2_H_
 
