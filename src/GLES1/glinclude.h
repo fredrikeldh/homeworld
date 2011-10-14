070e1e046a599a47c741771489a4ba12fa1c0b31
LCID Fire <lcid-fire@gmx.net>
2011-10-14 21:39:35 +0200

    Replace old HW_ENABLE_GLES1 by new HW_USE_GLES1

--- src/GLES1/glinclude.h
+++ src/GLES1/glinclude.h
@@ -1,12 +1,12 @@
 #ifndef _HW_GLES1_GLINCLUDE_H_
 #define _HW_GLES1_GLINCLUDE_H_
 
-#ifdef HW_ENABLE_GLES1
+#ifdef HW_USE_GLES1
 #include <GLES/egl.h>
 #include <GLES/glext.h>
 #else
 #error WRONG INCLUDE
-#endif //HW_ENABLE_GLES1
+#endif //HW_USE_GLES1
 
 #endif //_HW_GLES1_GLINCLUDE_H_
 
