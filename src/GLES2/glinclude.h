12100967dbe947d8d2d4af903d91c1053b7bb44f
LCID Fire <lcid-fire@gmx.net>
2011-10-21 00:47:51 +0200

    Use WGL instead of EGL when we're compiling for EMSCRIPTEN

--- src/GLES2/glinclude.h
+++ src/GLES2/glinclude.h
@@ -2,7 +2,11 @@
 #define _HW_GLES2_GLINCLUDE_H_
 
 #ifdef HW_USE_GLES2
+#ifdef EMSCRIPTEN
+#include <WGL/wgl.h>
+#else
 #include <EGL/egl.h>
+#endif //EMSCRIPTEN
 #include <GLES2/gl2.h>
 #include <GLES2/gl2platform.h>
 #include <GLES2/gl2ext.h>
