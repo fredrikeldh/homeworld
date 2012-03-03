03d700a488fe1323df7f0e5c9fd5a636d4f7d22e
LCID Fire <lcid-fire@gmx.net>
2012-03-03 02:46:50 +0100

    When using emscripten use egl headers

--- src/GLES2/glinclude.h
+++ src/GLES2/glinclude.h
@@ -2,11 +2,7 @@
 #define _HW_GLES2_GLINCLUDE_H_
 
 #ifdef HW_USE_GLES2
-#ifdef EMSCRIPTEN
-#include <WGL/wgl.h>
-#else
 #include <EGL/egl.h>
-#endif //EMSCRIPTEN
 #include <GLES2/gl2.h>
 #include <GLES2/gl2platform.h>
 #include <GLES2/gl2ext.h>
